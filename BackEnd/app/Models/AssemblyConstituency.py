b807b3e657d53275178b361304caec69604de3c3
Disha2109 <darshpreetsuri21@gmail.com>
2023-05-12 09:38:19 +0530

    adding requested changes

--- BackEnd/app/Models/AssemblyConstituency.py
+++ BackEnd/app/Models/AssemblyConstituency.py
@@ -5,9 +5,9 @@ class AssemblyConstituency(db.Model):
     __tablename__ = "AssemblyConstituency"
 
     Constituency_Id = db.Column(db.Integer, primary_key = True)
-    Constituency_Name = db.Column(db.String(100), unique = True)
+    Constituency_Name = db.Column(db.String(100), unique = True, null=False)
     Constituency_No = db.Column(db.Integer, null = False)
-    District_Code = db.Column(db.Integer, db.ForeignKey("Districts.District_Id"))
+    District_Code = db.Column(db.Integer, db.ForeignKey("Districts.District_Id"), null=False)
 
     def __init__(self, Constituency_Id, Constituency_Name, Constituency_No, District_Code):
         self.Constituency_Id = Constituency_Id
