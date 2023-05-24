b31823b4f0091ac006e2d2c07363b23729073108
shivakrishnaamandu <shivakrishna.amandu@gmail.com>
2023-05-24 21:26:26 +0530

    Added States modify and delete method

---
+++ BackEnd/app/APIs/Voters_API.py
@@ -0,0 +1,48 @@
+from Backend.app.__init__ import application, db
+from Backend.app.Models.Voters import *
+from Backend.app.Authentication.jwtservice import JWTService
+from Backend.app.Authentication.middleware import Middleware
+from flask import request, Blueprint
+import csv
+import uuid
+
+jwt_secret = "secret"
+
+jwt_service = JWTService(jwt_secret)
+middleware = Middleware(jwt_service)
+
+application.before_request(lambda: middleware.auth(request))
+
+Voters_API_blueprint = Blueprint("Voters_API", __name__)
+
+
+@Voters_API_blueprint.route('admin/upload_voters', methods=['POST'])
+def upload():
+    file = request.files['file']
+    if file:
+        csv_data = csv.reader(file)
+        next(csv_data)  # Skip header row if needed
+        for row in csv_data:
+            # Assuming the CSV columns are in the order of column1, column2
+            data = Voters(row)
+            db.session.add(data)
+            db.session.commit()
+        return 'File uploaded and data inserted into the database table successfully.'
+
+
+@Voters_API_blueprint.route("/admin/list_voters")
+def get_all_voters():
+    print(f"url accessed")
+    voters = Voters.query.all()
+    if voters:
+        voter_list = []
+        for voter in voters:
+            print(f"Voter Name: {voter.Voters_Name}")
+            voter_dict = {}
+            voter_dict["VoterId"] = voter.Voters_Id
+            voter_dict["VoterName"] = voter.Voters_Name
+            voter_dict["VoterNo"] = voter.Voters_No
+            voter_list.append(voter_dict)
+        return {"voters": voter_list}
+    else:
+        return {"message": "No voters Available"}
